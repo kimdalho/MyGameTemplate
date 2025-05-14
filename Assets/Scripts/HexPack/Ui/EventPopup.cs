441b2b3dbc2bb3d5c3370e1322ab5096e7a81ece
kimdalho <49323810+kimdalho@users.noreply.github.com>
2025-05-14 11:42:02 +0900

    저장소 초기화
    이전 포트폴리오 삭제

--- Assets/Scripts/HexPack/Ui/EventPopup.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EventPopup : BasePopup
-{
-
-    protected virtual IEnumerator CoAppearTitle(string title){ yield return null; }
-}
--- Assets/Scripts/ResearchContent/ResearchSO.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "RelicsSO", menuName = "Scriptable Object/Skill/ResearchSO")]
-public class ResearchSO : ScriptableObject
-{
-    public ResearchModel[] models;
-}
